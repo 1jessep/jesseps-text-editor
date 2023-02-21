ae18cb3fc10e40466af0b2e76fcd9f2b0c62d14e
1jessep <jessepstynchcomb@gmail.com>
2023-02-21 13:10:22 -0600

    final push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   const request = store.put({ id: 1, value: content });
 
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  console.log("Data saved to the database.", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
